35dd867d9ca21bbc4018b0a8770a8858c9e29911
alsolucionesweb <34529235+alejillo2501@users.noreply.github.com>
2025-04-05 01:00:08 -0500

    Change URI to production
    Change URI to production

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,8 +13,8 @@ export const appConfig: ApplicationConfig = {
 
       return {
         link: httpLink.create({
-          //uri: 'http://ec2-3-148-210-237.us-east-2.compute.amazonaws.com/graphql',
-          uri: 'http://localhost:8000/graphql',
+          uri: 'http://ec2-3-148-210-237.us-east-2.compute.amazonaws.com/graphql',
+          //uri: 'http://localhost:8000/graphql',
         }),
         cache: new InMemoryCache(),
       };
